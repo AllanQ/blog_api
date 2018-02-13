b14a3693f6e5b23e4cee69eec96ad91dc205860f
a <AllanQ1885@gmail.com>
2018-02-13 15:27:25 +0300

    disable some middlewares
    Rack::Sendfile
    ActionDispatch::Static
    ActionDispatch::Callbacks

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module BlogApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.delete ::Rack::Sendfile
+    config.middleware.delete ::ActionDispatch::Static
+    config.middleware.delete ::ActionDispatch::Callbacks
   end
 end
